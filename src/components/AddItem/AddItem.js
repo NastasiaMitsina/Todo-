11bb22333a81fa6ace521feb676859933e692a73
Anastasia Mitina <a.mitina@itransition.com>
2021-05-19 16:26:16 +0300

    feat: create onBubmit function

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -2,15 +2,37 @@ import React, { Component } from 'react';
 import './AddItem.css';
 
 export class AddItem extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            label: ''
+        }
+        
+        this.onLabelChange = (event) => {
+            this.setState({
+                label: event.target.value
+            })
+        }
+        this.onSubmit = (event) => {
+            event.preventDefault();
+            this.props.onAddToDo(this.state.label)
+        }
+    }
+
     render(){
-        const { onAddToDo } = this.props;
         return (
-            <div className='add-item'>
+            <form className='add-item d-flex' onSubmit={this.onSubmit}>
+                <input type='text' 
+                       className='form-control' 
+                       placeholder='What needs to be done'
+                       onChange={this.onLabelChange}
+                />
                 <button className='btn btn-outline-secondary'
-                        onClick={() => onAddToDo('Hello world')}>
+                        onClick={() => this.onSubmit}>
                     Add item
                 </button>
-            </div>
+            </form>
         )
     }
 }
